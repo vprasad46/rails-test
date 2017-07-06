32b6e827bc265c72da57fe8c54076655ea471460
vprasad46 <vprasad46@gmail.com>
2017-07-06 11:33:16 +0530

    Changes done

---
+++ test/system/static_pages_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class StaticPagesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit static_pages_url
+  #
+  #   assert_selector "h1", text: "StaticPage"
+  # end
+end
