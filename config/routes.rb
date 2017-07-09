5bc5101f5335625dd1fcd2810d5c036660583d52
vprasad46 <vprasad46@gmail.com>
2017-07-09 16:49:51 +0530

    Changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :users
   root 'static_pages#home'
 
   get 'users/new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705095122) do
+ActiveRecord::Schema.define(version: 20170709110847) do
 
   create_table "static_pages", force: :cascade do |t|
     t.string "home"
