5c4a7e08f6da829133c609cf708ee73732612132
vprasad46 <vprasad46@gmail.com>
2017-07-11 01:56:57 +0530

    Signing up process Complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   resources :users
   root 'static_pages#home'
 
-  get 'users/new'
+
   get  '/help',    to: 'static_pages#help' 
   get  '/about',   to: 'static_pages#about'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   		
 end
