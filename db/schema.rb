d0266b68e8d3410504fb50d90f37657221b1707d
vprasad46 <vprasad46@gmail.com>
2017-07-14 14:22:50 +0530

    index

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20170713170451) do
     t.string "remember_digest"
     t.boolean "admin"
     t.string "activation_digest"
-    t.boolean "activated"
+    t.boolean "activated", default: false
     t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
