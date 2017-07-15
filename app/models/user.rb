efc816a9298a0aa3742bbd36f289cf10fec43fea
Vishwa Prasad <vprasad46@gmail.com>
2017-07-16 00:21:21 +0530

    Tutorial completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,14 @@ class User < ApplicationRecord
     attr_accessor :remember_token, :activation_token
     before_save   :downcase_email
     has_many :microposts, dependent: :destroy
+    has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+    has_many :passive_relationships, class_name:  "Relationship",
+      foreign_key: "followed_id",
+      dependent:   :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships
     before_create :create_activation_digest
  	  validates :name,  presence: true, length: { maximum: 50 }
   	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -23,7 +31,23 @@ class User < ApplicationRecord
   end
 
    def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+ 
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   def remember
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit] 
   resources :microposts,only: [:create, :destroy]
+  resources :relationships,only: [:create, :destroy]
   root 'static_pages#home'
 
   get  '/help',    to: 'static_pages#help' 
@@ -13,4 +14,10 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  
 end
